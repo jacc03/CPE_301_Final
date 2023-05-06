2bb2951707c574b00b618d05a0f259916c629c98
rkrysinski1023 <70532714+rkrysinski1023@users.noreply.github.com>
2023-05-06 06:12:47 -0700

    Add files via upload
    Working program using only Arduino libraries for both LCD display and DHT sensor

--- Sensor_and_LCD.ino
+++ Sensor_and_LCD.ino
@@ -1,7 +1,20 @@
 #include <LiquidCrystal.h>
 
-uint8_t data[5];
-LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
+LiquidCrystal lcd(13,12,11,10,9,8);
+
+int humidity = 0;
+int temperature = 0;
+int temperaturecom = 0;
+bool DHTFailure = false;
+
+unsigned long ontime = 0;
+unsigned long offtime = 0;
+
+unsigned char datbyte[5];
+unsigned int datpacket[40];
+unsigned char bitdata = 0;
+unsigned char checksum = 0;
+int counter = 0;
 
 void setup(){
   Serial.begin(9600);
@@ -17,41 +30,44 @@ uint8_t Signal(bool pull){
 }
 
 void loop(){
-  //int value = DHT.read11(7);
-  //float humidity = DHT.humidity();
-  //float temp = DHT.temperature();
-  pinMode(7, INPUT_PULLUP);
-  delay(1000); // This allows us to wait between 1s measurements. 
   pinMode(7, OUTPUT);
   digitalWrite(7, LOW);
-  uint32_t tm = Signal(HIGH);  
-  tm = Signal(LOW);
-  tm = Signal(HIGH);
-  uint8_t signals[80];
-  for(uint8_t i = 0; i<80; i+=2){
-    signals[i] = Signal(LOW);  // there are low signals also
-    signals[i+1] = Signal(HIGH);  // our HIGH state signals
-  }
-  for (uint8_t a = 0; a<5; a++){
-    for (uint8_t i=0; i<16; i+=2){
-      data[a] <<= 1;
-      if(signals[16 * a + i] < signals[16*a+i+1]) data[a] |= 1;
+  delay(18);
+  digitalWrite(7, HIGH);
+  pinMode(7, INPUT_PULLUP);
+  offtime = pulseIn(7, LOW);
+  if (offtime <= 84 && offtime >= 76) {
+    while (true) {
+      ontime = pulseIn(7, HIGH);
+      if (ontime <= 28 && ontime >= 20) {
+        bitdata = 0;
+      }
+      else if (ontime <= 74 && ontime >= 65) {
+        bitdata = 1;
+      }
+      else if (counter == 40) {
+        break;
+      }
+      datbyte[counter / 8] |= bitdata << (7 - (counter % 8));
+      datpacket[counter] = bitdata;
+      counter++;
     }
   }
-  //This checks for the edge case 
-  if (isnan(data[0]) || isnan(data[2])){
+  checksum = datbyte[0] + datbyte[1] + datbyte[2] + datbyte[3];
+  if (checksum == datbyte[4] && checksum != 0) {
     lcd.setCursor(0,0);
-    lcd.print("ERROR: Read failed, please check sensor");
-  }
-  else{
-    lcd.setCursor(0,0);
-    lcd.print("Current Temperature: ");
-    lcd.print(data[0]);
+    lcd.print("Humidity: ");
+    lcd.print(datbyte[0]);
     lcd.print((char)223);
     lcd.setCursor(0,1);
-    lcd.print("Current Humidity:");
-    lcd.print(data[2]);
+    lcd.print("Temperature:");
+    lcd.print(datbyte[2]);
     lcd.print((char)223);
-    lcd.clear();
+    Serial.print(datbyte[1]);
+    delay(60000);
   }
+  counter = 0;
+  datbyte[0] = datbyte[1] = datbyte[2] = datbyte[3] = datbyte[4] = 0;
+  delay(1000);
+  //Serial.print(data[0]);
 }
